d0f342e7cab35019474eff34c6e8a7af13cb7363
Mikołaj Jóźwik <97754734+TheShadowKing19@users.noreply.github.com>
2023-01-16 22:19:59 +0100

    Zmiana w primary key i clustering order by

--- Skrypty/Lab10-Zad2.sql
+++ Skrypty/Lab10-Zad2.sql
@@ -9,5 +9,18 @@ create table tradeView (
     orderDate date,
     status varchar,
     wartoscZamowienia float,
-    primary key ( (employeeNumber), orderNumber )
-) with clustering order by (orderNumber DESC );
+    primary key ( (employeeNumber), customerNumber, orderNumber )
+) with clustering order by (customerNumber DESC );
+
+-- select
+--     employeeNumber,
+--     customerNumber,
+--     orderNumber,
+--     addressLine1,
+--     addressLine2,
+--     contactFirstName,
+--     contactLastName,
+--     orderDate,
+--     status,
+--     `Wartość zamówienia`
+-- from tradeView;
